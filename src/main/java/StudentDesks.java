d8eeaa2de25d5611e9610e4b8f797245333cad4d
Lanielle Pavlik <lanielle.222272@nv.ccsd.net>
2020-09-03 01:31:02 +0000

    StudentDesks class completed (probably).

--- src/main/java/StudentDesks.java
+++ src/main/java/StudentDesks.java
@@ -7,12 +7,24 @@ class StudentDesks {
         Scanner scanner = new Scanner(System.in);
 
         //Reads ints from user
+        System.out.println("Enter the number of students for each classroom:");
+        int students1 = scanner.nextInt();
+        int students2 = scanner.nextInt();
+        int students3 = scanner.nextInt();
+
+        //my code :)
+        int totalDesks = 0;
         
+        totalDesks += (students1 / 2);
+        if( (students1 % 2)!= 0 )totalDesks++;
 
-        /*
-         *  your code goes here
-         */
+        totalDesks += (students2 / 2);
+        if( (students2 % 2)!= 0 )totalDesks++;
 
+        totalDesks += (students3 / 2);
+        if( (students3 % 2)!= 0 )totalDesks++;
+
+        System.out.println(totalDesks+" desks");
 
         // closing the scanner object
         scanner.close();
